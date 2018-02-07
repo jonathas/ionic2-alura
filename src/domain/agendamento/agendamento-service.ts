3b1a417960d739f577085b29f1b5dd67fe61ee14
Jon Ribeiro <contact@jonathas.com>
2018-02-07 23:33:34 +0100

    Implemented resending of schedules

--- src/domain/agendamento/agendamento-service.ts
+++ src/domain/agendamento/agendamento-service.ts
@@ -7,17 +7,29 @@ import { AgendamentoDao } from './agendamento-dao';
 export class AgendamentoService {
   constructor(private http: Http, private dao: AgendamentoDao) { }
 
-  agenda(agendamento: Agendamento) {
-    let url = `https://aluracar.herokuapp.com/salvarpedido?carro=${agendamento.carro.nome}&nome=${agendamento.nome}&preco=${agendamento.valor}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+  private montaUri(agendamento: Agendamento) {
+    return `https://aluracar.herokuapp.com/salvarpedido?carro=${agendamento.carro.nome}&nome=${agendamento.nome}&preco=${agendamento.valor}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+  }
 
+  agenda(agendamento: Agendamento) {
     return this.dao.isAgendamentoDuplicado(agendamento).then(existe => {
       if (existe) throw new Error('Esse agendamento já foi realizado');
       return this.http.
-        get(url)
+        get(this.montaUri(agendamento))
         .toPromise()
         .then(() => agendamento.confirmado = true, err => console.error(err))
         .then(() => this.dao.salva(agendamento))
         .then(() => agendamento.confirmado);
-    })
+    });
   }
+
+  reagenda(agendamento: Agendamento) {
+    return this.http.
+      get(this.montaUri(agendamento))
+      .toPromise()
+      .then(() => agendamento.confirmado = true, err => console.error(err))
+      .then(() => this.dao.salva(agendamento))
+      .then(() => agendamento.confirmado);
+  }
+
 }
--- src/pages/agendamentos/agendamentos.ts
+++ src/pages/agendamentos/agendamentos.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { AgendamentoDao } from './../../domain/agendamento/agendamento-dao';
 import { Agendamento } from '../../domain/agendamento/agendamento';
+import { AgendamentoService } from './../../domain/agendamento/agendamento-service';
 
 @Component({
   selector: 'page-agendamentos',
@@ -14,10 +15,29 @@ export class AgendamentosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private dao: AgendamentoDao) {
+    private dao: AgendamentoDao,
+    private service: AgendamentoService,
+    private alertCtrl: AlertController) {
 
     this.dao.listaTodos()
-    .then(agendamentos => this.agendamentos = agendamentos);
+      .then(agendamentos => this.agendamentos = agendamentos);
+  }
+
+  reenvia(agendamento: Agendamento) {
+    this.service.reagenda(agendamento)
+      .then(confirmado => {
+        confirmado
+          ? this.alertCtrl.create({
+            title: 'Envio',
+            subTitle: 'Agendamento reenviado com sucesso',
+            buttons: [{ text: 'Ok' }]
+          }).present()
+          : this.alertCtrl.create({
+            title: 'Envio',
+            subTitle: 'Não foi possível reenviar o agendamento. Tente outra vez',
+            buttons: [{ text: 'Ok' }]
+          }).present()
+      });
   }
 
 }
